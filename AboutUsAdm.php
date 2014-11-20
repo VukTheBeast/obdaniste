54974b275f8b3fdea2ceb3e1c8c26ecd95e5227d
drasko <dragomix2001@gmail.com>
2014-11-20 12:50:24 +0100

    proba

--- AboutUsAdm.php
+++ AboutUsAdm.php
@@ -3,7 +3,7 @@ session_start();
 //echo $_SESSION["myusername"];
 if($_SESSION["myusername"] != "drasko"){
 	header("Location: /partial/admin.php");
-    //echo "<script>window.location.assign('/partial/admin.php')</script>";
+    //echo "<script>window.location.assign('/partial/admin.php')</script>";proba
 }
 ?>
 <!DOCTYPE html>
